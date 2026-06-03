37b3e5f28c67a52652b6fc9b6413c51e347901a9
Roberto <robertoo.jof@gmail.com>
2026-06-02 23:28:29 -0300

    feat: Configura estrutura inicial do backend

---
+++ src/backend/marketplace-mps-api/src/test/java/com/ufpb/mps/makertplace/MarketplaceMpsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufpb.mps.makertplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceMpsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
