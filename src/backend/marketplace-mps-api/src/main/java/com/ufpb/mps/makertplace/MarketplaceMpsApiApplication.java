d1b617348a63dae4826be38201703bfbdd3b141b
Roberto <robertoo.jof@gmail.com>
2026-06-02 23:28:29 -0300

    feat: Configura estrutura inicial do backend

---
+++ src/backend/marketplace-mps-api/src/main/java/com/ufpb/mps/makertplace/MarketplaceMpsApiApplication.java
@@ -0,0 +1,13 @@
+package com.ufpb.mps.makertplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceMpsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceMpsApiApplication.class, args);
+	}
+
+}
