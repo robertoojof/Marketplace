4d3f153a8af3a0efaa551030ded780fd72629bf0
Roberto <robertoo.jof@gmail.com>
2026-06-10 23:33:27 -0300

    feat: fallback da versão do Java para 21 e adiciona dependências do Spring Modulith

--- src/backend/marketplace-mps-api/src/test/java/com/ufpb/mps/makertplace/MarketplaceMpsApiApplicationTests.java
+++ src/backend/marketplace-mps-api/src/test/java/com/ufpb/mps/makertplace/MarketplaceMpsApiApplicationTests.java
@@ -2,12 +2,23 @@ package com.ufpb.mps.makertplace;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class MarketplaceMpsApiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void verificaArquiteturaModular() {
+		ApplicationModules modules = ApplicationModules.of(MarketplaceMpsApiApplication.class);
+
+		System.out.println("Quantidade de modulos: " + modules.stream().count());
+		modules.forEach(module -> System.out.println("Modulo: " + module.getName()));
+		modules.verify();
+	}
 }
