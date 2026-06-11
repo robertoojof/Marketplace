94fd99187d2754df6b15fb7060c5e8184d163272
Roberto <robertoo.jof@gmail.com>
2026-06-10 22:57:04 -0300

    feat: adiciona configuração do Swagger e segurança para a API

---
+++ src/backend/marketplace-mps-api/src/main/java/com/ufpb/mps/makertplace/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.ufpb.mps.makertplace.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.disable()) // Desabilita CSRF
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(
+                "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html")
+            .permitAll()
+            // Define que qualquer outra rota precisa de autenticação
+            .anyRequest().authenticated());
+
+    return http.build();
+  }
+}
